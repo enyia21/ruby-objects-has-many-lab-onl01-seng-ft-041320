b2c8e00d2bedceb98218450dab0555d82ca4cae2
Chibuisi Enyia <enyia21@gmail.com>
2020-04-28 20:22:04 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -2,10 +2,17 @@ class Post
 
   @@all = []
 
-  attr_accessor :title
+  attr_accessor :title, :author
 
   def initialize(title)
     @title = title
     @@all << self
+
+  def self.all
+    @@all
   end
+
+  # def author 
+  #   
+  # end 
 end
